67489edb65d881f3e50533e0149f931cb872be0e
Soonmok <tnsahr2580@gmail.com>
2019-06-24 21:41:01 +0900

    apply mecab

--- tokenization.py
+++ tokenization.py
@@ -17,6 +17,7 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+from konlpy.tag import Mecab
 
 import collections
 import re
@@ -137,6 +138,10 @@ def convert_by_vocab(vocab, items):
   """Converts a sequence of [tokens|ids] using the vocab."""
   output = []
   for item in items:
+    if item == '\xa0':
+      print("i got xa0")
+      output.append(85949)
+      continue
     output.append(vocab[item])
   return output
 
@@ -166,13 +171,14 @@ class FullTokenizer(object):
     self.inv_vocab = {v: k for k, v in self.vocab.items()}
     self.basic_tokenizer = BasicTokenizer(do_lower_case=do_lower_case)
     self.wordpiece_tokenizer = WordpieceTokenizer(vocab=self.vocab)
+    self.mecab_tokenizer = Mecab('../mecab-ko-dic-2.1.1-20180720')
 
   def tokenize(self, text):
     split_tokens = []
-    for token in self.basic_tokenizer.tokenize(text):
-      for sub_token in self.wordpiece_tokenizer.tokenize(token):
-        split_tokens.append(sub_token)
-
+    for token in self.mecab_tokenizer.morphs(text):
+      #for sub_token in self.wordpiece_tokenizer.tokenize(token):
+      #print(token)
+      split_tokens.append(token)
     return split_tokens
 
   def convert_tokens_to_ids(self, tokens):
